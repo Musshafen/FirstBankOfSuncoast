7aa232df1441ff672940a2a6fea1f284f135a9db
KM Musshafen <kmusshafen@hotmail.com>
2023-03-31 18:57:05 -0400

    Create description

--- Transaction.cs
+++ Transaction.cs
@@ -7,5 +7,17 @@ namespace FirstBankOfSuncoast
         public int Amount { get; set; }
 
         public string Type { get; set; }
+
+
+        public string Description()
+        {
+            var descriptionString = $"{Type} of ${Amount} to {Account}";
+
+            return descriptionString;
+
+
+
+        }
+
     }
 }
